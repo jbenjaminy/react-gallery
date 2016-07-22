f831c44d04dbaa25f14110416f97d0e50bd7339e
Sierra Gregg <sierragregg@Gmail.com>
2016-07-22 13:32:36 -0500

    Added tests to gallary

--- test/test-gallery.jsx
+++ test/test-gallery.jsx
@@ -3,6 +3,7 @@ var TestUtils = require('react-addons-test-utils');
 var should = require('chai').should();
 
 var Gallery = require('../js/gallery');
+var Image = require('../js/image');
 
 describe('Gallery component', function() {
     it('Displays a series of images',  function() {
@@ -15,44 +16,20 @@ describe('Gallery component', function() {
     var renderer = TestUtils.createRenderer();
     renderer.render(<Gallery images={images}/>);
     var result = renderer.getRenderOutput();
-    console.log(result, '<result');
 
     result.props.className.should.equal('gallery');
     result.type.should.equal('div');
     result.props.children.length.should.equal(2);
 
-    var img1 = result.props.children[0].props.children[0];
-    console.log(img1, '<img1');
-	// img1.props.children[0].type.should.equal('img');
-    img1.src.should.equal(images[0].url);
-    img1.alt.should.equal(images[0].description);
-    img1.key.should.equal('0');
-    img1.children[1].type.should.equal('p');
+    var img1 = result.props.children[0];
+    img1.type.should.equal(Image);
+    img1.props.url.should.equal(images[0].url);
+    img1.props.description.should.equal(images[0].description);
 
-    var img2 = result.props.children[1].props.children[0];
-    console.log(img2, '<img2');
-	// img2.props.children[0].type.should.equal('img');
-    img2.src.should.equal(images[1].url);
-    img2.alt.should.equal(images[1].description);
-    img2.key.should.equal('1');
-    img2.children[1].type.should.equal('p');
+    var img2 = result.props.children[1];
+    img2.type.should.equal(Image);
+    img2.props.url.should.equal(images[1].url);
+    img2.props.description.should.equal(images[1].description);
+
 	});
 });
-
-//  Gallery component
-// { className: 'gallery',
-//   children: 
-//    [ { '$$typeof': Symbol(react.element),
-//        type: [Function: Image],
-//        key: '0',
-//        ref: null,
-//        props: [Object],
-//        _owner: null,
-//        _store: {} },
-//      { '$$typeof': Symbol(react.element),
-//        type: [Function: Image],
-//        key: '1',
-//        ref: null,
-//        props: [Object],
-//        _owner: null,
-//        _store: {} } ] } '<result.props'
